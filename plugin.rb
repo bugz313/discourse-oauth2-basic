a85b4bd0de436df4949de09e23dee571ac3b54ee
Eduard Reichenberger <eduard@champion.systems>
2019-07-25 15:49:12 +0300

    Activate user

--- plugin.rb
+++ plugin.rb
@@ -188,6 +188,7 @@ class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
       log(">>>>>>>>> creating user #{result.name}, #{result.email}, #{result.username}, #{admin}, #{moderator}")
       result.user = User.create!(name: result.name, email: result.email, username: result.email.split("@")[0].slice(0,20), admin: admin, moderator: moderator)
       result.user.update!(approved: true)
+      result.user.update!(active: true)
       log(">>>>>>>>> user created #{result.user.id}")
     end
 
